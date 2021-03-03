8689729bc4acde9138f968e1de0477f4014a1fb8
annavasylashko <anna.vasylashko123@gmail.com>
2021-03-03 22:25:46 +0200

    Finished app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import CardColumn from './components/CardColumn/CardColumn'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CardColumn value="Good things" cardColor="#bb86fc"/>
+      <CardColumn value="Bad things" cardColor="#ff7597"/>
+      <CardColumn value="Action items" cardColor="#02ddc5"/>
     </div>
   );
 }
